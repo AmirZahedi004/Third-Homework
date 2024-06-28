e918c106b4c9adbf2430c83e6a4060dd7868e2a2
Amir Zahedi <152645437+AmirZahedi004@users.noreply.github.com>
2024-06-28 06:57:47 -0700

    Add files via upload

---
+++ Third-Homework/Third-Homework.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+        
+}
+int func(){
+    for(int i = 1 ; i <= 100 ; i++){
+        printf("%i" , i);
+    };
+    for(int a = 100 ; a >= 1 ; a--){
+        printf("%i" , a);
+    };
+    for(int s = 6 ; s <= 66 ; s += 6){
+        printf("%i" , s);
+    };
+    for(int f = 30 ; f >= 3 ; f -= 3){
+        printf("%i" , f);
+    };
+    for(int q = 2 ; q <= 17 ; q += 3){
+        printf("%i" , q);
+    };
+    for(int z = 44 ; z >= 0 ; z -= 11){
+        printf("%i" , z);
+    };
+}
